c6d726cb9e301711fbcb5bd1e2046bb0240edb8e
Darci <darcinevitt@gmail.com>
2024-12-08 23:13:05 -0500

    installing matplotlib for visualization, continuing rarity analysis

--- load_python.py
+++ load_python.py
@@ -4,7 +4,7 @@ import pandas as pd
 def load_data(data_folder='data'): # load all CSV files in the data folder.
     all_data = {}
     for file in os.listdir(data_folder): #loop through all files.
-        if file.endswith('.csv'):
+        if file.endswith('2.csv'):
             catalog_name = file.replace('.csv', '') #maintain file name (but drop '.csv') as the dictionary key.
             df = pd.read_csv(os.path.join(data_folder, file)) #convert csv into dataframe.
             all_data[catalog_name] = df 
